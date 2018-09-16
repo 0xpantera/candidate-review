79297e313b7aa402f581e9a7fef7f82b656b8772
Francososa <franky.sosa@gmail.com>
2018-09-16 12:30:02 -0600

    Define function to evaluate candidates viability

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,19 @@ module Lib
     ( someFunc
     ) where
 
+import Lib.Types
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+viable :: Candidate -> Bool
+viable candidate = all (== True) tests
+  where passedCoding = codeReview candidate > B
+        passedCultureFit = cultureFit candidate > C
+        educationMin = education candidate >= MS
+        tests = [passedCoding
+                ,passedCultureFit
+                ,educationMin]
+
+
+                               
