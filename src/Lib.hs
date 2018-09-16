43870ad824cd3cb325851ee986c499f12cb318d7
Francososa <franky.sosa@gmail.com>
2018-09-16 13:10:54 -0600

    Define main function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,8 @@
 module Lib
-    ( someFunc
+    ( assessCandidateIO
     ) where
 
 import Lib.Types
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
 
 viable :: Candidate -> Bool
 viable candidate = all (== True) tests
@@ -25,3 +22,28 @@ readGrade = getLine >>= (return . read)
 
 readDegree :: IO Degree
 readDegree = getLine >>= (return . read)
+
+readCandidate :: IO Candidate
+readCandidate = do
+  putStrLn "Enter id:"
+  cId <- readInt
+  putStrLn "enter code grade:"
+  codeGrade <- readGrade
+  putStrLn "Enter culture fit grade:"
+  cultureGrade <- readGrade
+  putStrLn "enter education:"
+  degree <- readDegree
+  return (Candidate { candidateId = cId
+                    , codeReview = codeGrade
+                    , cultureFit = cultureGrade
+                    , education = degree })
+
+
+assessCandidateIO :: IO ()
+assessCandidateIO = do
+  candidate <- readCandidate
+  let passed = viable candidate
+  let statement = if passed
+                  then "passed"
+                  else "failed"
+  putStrLn statement
