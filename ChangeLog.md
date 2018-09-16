bfd046a749287a7d4ad69bc0a3f97a8e000700f4
Francososa <franky.sosa@gmail.com>
2018-09-16 12:16:47 -0600

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for candidate-review
+
+## Unreleased changes
---
+++ src/Lib/Types.hs
@@ -0,0 +1,12 @@
+module Lib.Types where
+
+
+data Grade = F | D | C | B | A deriving (Eq, Ord, Enum, Show, Read)
+
+data Degree = HS | BA | MS | PhD deriving (Eq, Ord, Enum, Show, Read)
+
+data Candidate = Candidate
+  { candidateId :: Int
+  , codeReview :: Grade
+  , cultureFit :: Grade
+  , education :: Degree } deriving Show
